fac3031f18e59b4e5d5505ca3da202225d2c2c6e
Alan Paiva <jpaiva.alan@gmail.com>
2023-06-25 21:28:15 -0300

    Add swift-sass dependency

--- Package.swift
+++ Package.swift
@@ -1,21 +1,27 @@
 // swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DartSassPublishPlugin",
+    platforms: [
+        .macOS(.v11)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DartSassPublishPlugin",
             targets: ["DartSassPublishPlugin"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/johnfairh/swift-sass.git", from: "1.7.0"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DartSassPublishPlugin"),
+            name: "DartSassPublishPlugin",
+            dependencies: [
+                .product(name: "DartSass", package: "swift-sass")
+            ]
+        ),
         .testTarget(
             name: "DartSassPublishPluginTests",
             dependencies: ["DartSassPublishPlugin"]),
